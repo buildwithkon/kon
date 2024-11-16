7a79c4284d1442c2c4948e53f901a54a76fe806d
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-11-16 08:57:22 +0700

    deploy AppPoint

--- packages/contracts/script/AppPoint.s.sol
+++ packages/contracts/script/AppPoint.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.27;
 import "forge-std/Script.sol";
 import "../src/AppPointFactory.sol";
 
-contract AppPointScript is Script {
+contract Deploy is Script {
     function run() external {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
@@ -14,7 +14,7 @@ contract AppPointScript is Script {
         AppPointFactory factory = AppPointFactory(factoryAddress);
 
         // Deploy a new AppPoint using the factory
-        factory.createAppPoint("KonAppPoint", "KON");
+        factory.createAppPoint("KonPoint", "KON");
 
         vm.stopBroadcast();
     }
