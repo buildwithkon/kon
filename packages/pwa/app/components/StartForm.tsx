164d5dbe40acbeba8f891f1ef409204a83e61ca9
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-11-17 01:44:29 +0700

    subname form

--- packages/pwa/app/components/StartForm.tsx
+++ packages/pwa/app/components/StartForm.tsx
@@ -1,38 +1,37 @@
-import { useForm } from 'react-hook-form'
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { getFormProps, getInputProps, useForm } from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
+import { Form } from '@remix-run/react'
+import { z } from 'zod'
 
-const NameFormSchema = z.object({
-  name: z.string()
-  .min(3, { message: "Name must be at least 3 characters long." })
-  .max(255, { message: "Name cannot exceed 255 characters." })
-  .regex(/^[a-z0-9-]+$/, { message: "Name can only contain lowercase letters, numbers, and hyphens." })
-  .refine(name => !name.startsWith('-') && !name.endsWith('-'), {
-    message: "Name cannot start or end with a hyphen."
-  })
+export const schema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'Name must be at least 3 characters long.' })
+    .max(255, { message: 'Name cannot exceed 255 characters.' })
+    .regex(/^[a-z0-9-]+$/, { message: 'Name can only contain lowercase letters, numbers, and hyphens.' })
+    .refine((name) => !name.startsWith('-') && !name.endsWith('-'), {
+      message: 'Name cannot start or end with a hyphen.'
+    })
 })
-type NameFormSchemaType = z.infer<typeof NameFormSchema>;
 
 export default function StartForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<NameFormSchemaType>({ resolver: zodResolver(NameFormSchema) })
-
-  const onSubmit = async (data: NameFormSchemaType) => {
-    console.log('form:::', data)
-  }
+  const [form, fields] = useForm({
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onBlur',
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema })
+    }
+  })
 
   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-4">
-          <input {...register('name')} className="w-full" placeholder="yourname" />
-          {errors.name && <span className="text-sm px-1.5 text-red-400">{errors.name.message}</span>}
-          <button type="submit" className="btn-main w-full">
-            Claim your name
-          </button>
-        </div>
-    </form>
+    <Form method="post" {...getFormProps(form)}>
+      <div className="space-y-4">
+        <input {...getInputProps(fields.name, { type: 'text' })} className="w-full" placeholder="yourname" />
+        {fields.name.errors && <span className="px-1.5 text-red-400 text-sm">{fields.name.errors[0]}</span>}
+        <button type="submit" className="btn-main w-full">
+          Set yourname
+        </button>
+      </div>
+    </Form>
   )
 }
--- packages/pwa/app/routes/start.tsx
+++ packages/pwa/app/routes/start.tsx
@@ -1,13 +1,27 @@
-import type { MetaFunction } from '@remix-run/cloudflare'
+import { parseWithZod } from '@conform-to/zod'
+import type { ActionFunction, MetaFunction } from '@remix-run/cloudflare'
 import { useAtomValue } from 'jotai'
 import { loaderDataAtom } from '~/atoms'
 import StartForm from '~/components/StartForm'
+import { schema } from '~/components/StartForm'
 import TopBar from '~/components/TopBar'
 import IconKon from '~/components/icon/kon'
 
 export const meta: MetaFunction = () => {
   const ld = useAtomValue(loaderDataAtom)
   return [{ title: `Sart | ${ld?.appConfig?.name ?? 'KON'}` }]
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+  const submission = parseWithZod(formData, { schema })
+
+  if (submission.status !== 'success') {
+    return submission.reply()
+  }
+
+  console.log(submission.value)
+  return submission.value
 }
 
 export default function Start() {
@@ -18,18 +32,18 @@ export default function Start() {
       <TopBar>
         <div className="flex w-full justify-center">{ld?.appConfig?.name}</div>
       </TopBar>
-        <div className="flex items-center justify-center">
-          {ld?.appConfig?.icons?.logo ? (
-            <img
-              src={ld?.appConfig?.icons?.logo}
-              className="mb-8 max-h-48 max-w-48 rounded-full"
-              alt={ld?.appConfig?.name ?? ''}
-            />
-          ) : (
-            <IconKon size={198} className="mb-8" />
-          )}
-        </div>
-        <StartForm />
+      <div className="flex items-center justify-center">
+        {ld?.appConfig?.icons?.logo ? (
+          <img
+            src={ld?.appConfig?.icons?.logo}
+            className="mb-8 max-h-48 max-w-48 rounded-full"
+            alt={ld?.appConfig?.name ?? ''}
+          />
+        ) : (
+          <IconKon size={198} className="mb-8" />
+        )}
       </div>
+      <StartForm />
+    </div>
   )
 }
