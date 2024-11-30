940e8c415abbb86def75ebde0256f9920914f2c5
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-12-01 00:10:39 +0900

    recactor

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,60 +1,7 @@
 import { Hono } from 'hono'
-import { cache } from 'hono/cache'
-import { normalize } from 'viem/ens'
-import { getClient } from './client'
-
-const ENS_APPCONFIG_NAME = 'kon.eth'
-const ENS_APPCONFIG_KEY = 'app.kon'
+import ens from './ens'
 
 const app = new Hono()
-
-app.get(
-  '*',
-  cache({
-    cacheName: 'kon-api-ens',
-    cacheControl: 'max-age=600'
-  })
-)
-
-app.get('/ens/:chain/getAppConfig/:id', async (c) => {
-  const id = c.req.param('id')
-  const chain = c.req.param('chain') as 'mainnet' | 'sepolia'
-  const client = getClient(chain, c.env.ALCHEMY_API_KEY)
-  const res = await client.getEnsText({
-    name: normalize(`${id}.${ENS_APPCONFIG_NAME}`),
-    key: ENS_APPCONFIG_KEY
-  })
-  return c.json(res)
-})
-
-app.get('/ens/:chain/getAppAvatar/:id', async (c) => {
-  const id = c.req.param('id')
-  const chain = c.req.param('chain') as 'mainnet' | 'sepolia'
-  const client = getClient(chain, c.env.ALCHEMY_API_KEY)
-  const res = await client.getEnsAvatar({
-    name: normalize(`${id}.${ENS_APPCONFIG_NAME}`)
-  })
-  return c.json(res)
-})
-
-app.get('/ens/:chain/getSubname/:address', async (c) => {
-  const address = c.req.param('address') as `0x${string}`
-  const chain = c.req.param('chain') as 'mainnet' | 'sepolia'
-  const client = getClient(chain, c.env.ALCHEMY_API_KEY)
-  const res = await client.getEnsName({
-    address
-  })
-  return c.json(res)
-})
-
-app.get('/ens/:chain/getSubnameAddress/:name', async (c) => {
-  const name = c.req.param('name')
-  const chain = c.req.param('chain') as 'mainnet' | 'sepolia'
-  const client = getClient(chain, c.env.ALCHEMY_API_KEY)
-  const res = await client.getEnsAddress({
-    name: normalize(name)
-  })
-  return c.json(res)
-})
+app.route('/ens', ens)
 
 export default app
