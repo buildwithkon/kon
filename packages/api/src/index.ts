35b5512fa29401668f5ffd8bed08a8928cb5d60e
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-12-03 17:42:34 +0900

    add root text

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,5 +4,9 @@ import ens from './ens'
 const app = new Hono<{ Bindings: Env }>()
 app.route('/ens', ens)
 
+app.get('/', (c) => {
+  return c.text('kon.xyz API')
+})
+
 export type ApiType = typeof app
 export default app
