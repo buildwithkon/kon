af17ba854483340bdf247e1fea8e35eefbf5bff5
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-12-10 10:39:07 +0900

    form

---
+++ packages/pwa/app/components/RegistarForm.tsx
@@ -0,0 +1,71 @@
+import { useForm } from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
+import { Form } from '@remix-run/react'
+import { z } from 'zod'
+
+export const FormSchema = z.object({
+  id: z
+    .string()
+    .min(3, { message: 'ID must be at least 3 characters long.' })
+    .max(255, { message: 'ID cannot exceed 255 characters.' })
+    .regex(/^[a-z0-9-]+$/, { message: 'ID can only contain lowercase letters, numbers, and hyphens.' })
+    .refine((name) => !name.startsWith('-') && !name.endsWith('-'), {
+      message: 'ID cannot start or end with a hyphen.'
+    }),
+  name: z
+    .string()
+    .max(255, { message: 'Display Name cannot exceed 255 characters.' })
+    .or(
+      z
+        .string()
+        .optional()
+        .transform((value) => value ?? '')
+    )
+})
+export type FormSchemaType = z.infer<typeof FormSchema>
+
+export default function RegistorForm() {
+  const [form, fields] = useForm({
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: FormSchema })
+    },
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onInput'
+  })
+
+  return (
+    <Form method="POST" id={form.id} onSubmit={form.onSubmit} noValidate>
+      <div className="space-y-4">
+        <div>
+          <input
+            key={fields.id.key}
+            name={fields.id.name}
+            className="w-full"
+            placeholder="Your ID"
+            aria-describedby="subname"
+          />
+          <small id="subname" className="px-2 pt-0.5 text-sm">
+            * Only lowercase letters, numbers, and hyphens.
+          </small>
+          {fields.id.errors && <p className="px-1.5 text-red-400 text-sm">{fields.id.errors}</p>}
+        </div>
+        <div>
+          <input
+            key={fields.name.key}
+            name={fields.name.name}
+            className="w-full"
+            placeholder="Display Name"
+            aria-describedby="displayname"
+          />
+          <small id="displayname" className="px-2 pt-0.5 text-sm">
+            * Optional, can be changed later.
+          </small>
+          {fields.name.errors && <p className="px-1.5 text-red-400 text-sm">{fields.name.errors}</p>}
+        </div>
+        <button type="submit" className="btn-main w-full">
+          Join
+        </button>
+      </div>
+    </Form>
+  )
+}
--- packages/pwa/app/routes/start.tsx
+++ packages/pwa/app/routes/start.tsx
@@ -1,7 +1,10 @@
-import type { MetaFunction } from '@remix-run/cloudflare'
-import { useRouteLoaderData } from '@remix-run/react'
+import { parseWithZod } from '@conform-to/zod'
+import type { ActionFunctionArgs, LoaderFunction, MetaFunction } from '@remix-run/cloudflare'
+import { useLoaderData, useRouteLoaderData } from '@remix-run/react'
 import ProfileCard from '~/components/ProfileCard'
-import StartForm from '~/components/StartForm'
+import RegistarForm from '~/components/RegistarForm'
+import { FormSchema } from '~/components/RegistarForm'
+import { genRanStr } from '~/lib/utils'
 import type { RootLoader } from '~/root'
 import type { LoaderData } from '~/types'
 
@@ -10,16 +13,78 @@ export const meta: MetaFunction = ({ matches }) => {
   return [{ title: `Start | ${ld?.appConfig?.name ?? ''}` }]
 }
 
+export const loader: LoaderFunction = () => ({
+  str1: genRanStr(),
+  str2: genRanStr(10)
+})
+
+export const action = async ({ request, context }: ActionFunctionArgs) => {
+  const formData = await request.formData()
+  const submission = parseWithZod(formData, { schema: FormSchema })
+
+  if (submission.status !== 'success') {
+    return submission.reply()
+  }
+
+  console.log(
+    '-------2',
+    context,
+    `${new URL(request.url).origin}/ens/sepolia/getSubnameAddress/${submission.value.id}`
+  )
+  const res = await context.cloudflare.env.API_WORKER.fetch(
+    `${new URL(request.url).origin}/ens/sepolia/getSubnameAddress/${submission.value.id}`
+  ).then((res) => res.json())
+
+  console.log('-------3')
+  const checkId = JSON.parse(res)
+  console.log('-------4')
+
+  // const checkId = await getSubnameAddress(
+  //   ld.ENV,
+  //   `${submission.value.id}.${ld.subdomain}.${ENS_APPCONFIG_NAME}`
+  // )
+
+  console.log('--------submission::', submission.value, '--------checkId::', checkId)
+
+  // check alredy exists
+  if (checkId) {
+    return submission.reply({
+      formErrors: ['ID already exists']
+    })
+  }
+
+  // if (chainId !== baseSepolia.id) {
+  //   await switchChain({ chainId: baseSepolia.id })
+  // }
+
+  // try {
+  //   const res = await writeContractAsync({
+  //     abi,
+  //     address: REGISTAR_ADDRESS,
+  //     functionName: 'register',
+  //     args: [normalize(data.id), address as `0x${string}`]
+  //   })
+  //   console.log('res:::', res)
+  // } catch (error: any) {
+  //   console.log(error)
+  // }
+
+  return Response.json({
+    success: true
+  })
+}
+
 export default function Start() {
   const ld = useRouteLoaderData<RootLoader>('root')
+  const { str1, str2 } = useLoaderData<typeof loader>()
 
   return (
     <div className="wrapper-app-full">
-      <ProfileCard qr={false} />
-      <p className="px-4 pt-6 pb-10 text-xl">{ld?.appConfig?.description}</p>
-      <hr />
+      <ProfileCard qr={false} point={str1} name={str2} />
+      <p className="px-4 pt-6 pb-8 text-xl">{ld?.appConfig?.description}</p>
+      <p className="text-center text-xl">⬇</p>
       <h1 className="-mx-1 pt-10 pb-6 text-center font-bold text-2xl">👋 Join “{ld?.appConfig?.name}”</h1>
-      <StartForm />
+      <RegistarForm />
     </div>
   )
 }
