1746856edf8dcc78a83b5e72789cc5be8966941b
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-12-16 09:46:36 +0900

    chore

--- packages/pwa/app/components/AppProviders.tsx
+++ packages/pwa/app/components/AppProviders.tsx
@@ -1,7 +1,7 @@
 import { useRouteLoaderData } from '@remix-run/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider as JotaiProvider } from 'jotai'
-import { type ReactNode, useState } from 'react'
+import { type ReactNode, useMemo, useState } from 'react'
 import { WagmiProvider } from 'wagmi'
 import { cookieToInitialState } from 'wagmi'
 import { store } from '~/atoms'
@@ -16,7 +16,7 @@ export default function AppProviders({
   const ld = useRouteLoaderData<RootLoader>('root')
   const [config] = useState(() => getConfig(ld?.ENV))
   const [queryClient] = useState(() => new QueryClient())
-  const [initialState] = useState(() => cookieToInitialState(config, ld.cookie))
+  const initialState = useMemo(() => cookieToInitialState(config, ld.cookie), [config, ld.cookie])
 
   return (
     <JotaiProvider store={store}>
