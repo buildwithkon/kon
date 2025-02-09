a7a3a1de01a144163ae79b066833d4e36478efa3
Yuji Yamaguchi <yuji.ym@gmail.com>
2025-02-10 02:09:58 +0900

    remove packages/pwa

--- packages/pwa/app/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { SpinnerGap } from '@phosphor-icons/react'
-
-export default function Loading() {
-  return (
-    <div className="fixed top-0 right-0 bottom-0 left-0 z-50 grid place-items-center bg-gray-500/60 backdrop-blur">
-      <SpinnerGap weight="bold" size={80} className="animate-spin-slow" />
-    </div>
-  )
-}
