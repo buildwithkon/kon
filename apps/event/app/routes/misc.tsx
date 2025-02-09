4baea22ceaaef1f958c8489198cad2332feacb49
Yuji Yamaguchi <yuji.ym@gmail.com>
2025-02-10 00:25:40 +0900

    update ui

--- apps/event/app/routes/misc.tsx
+++ apps/event/app/routes/misc.tsx
@@ -1,19 +1,31 @@
 import BottomBar from '@konxyz/shared-react/components/BottomBar'
 import TopBar from '@konxyz/shared-react/components/TopBar'
+import Markdown from '@konxyz/shared-react/components/modules/Markdown'
 import { mergeMeta } from '@konxyz/shared/lib/remix'
-import { useRouteLoaderData } from 'react-router'
+import { useLoaderData, useRouteLoaderData } from 'react-router'
 import type { Route } from './+types/misc'
 
 export const meta = mergeMeta(({ matches }: Route.MetaArgs) => [
   { title: `Misc | ${matches[0]?.data?.appConfig?.name ?? ''}` }
 ])
 
+export const loader = async ({}: Route.LoaderArgs) => {
+  const res = await fetch('https://hackmd.io/@yujiym/BkOGAVIt1g/download')
+  const content = await res.text()
+
+  return {
+    content
+  }
+}
+
 export default function Home() {
   const ld = useRouteLoaderData('root')
+  const { content } = useLoaderData()
 
   return (
-    <div className="wrapper-app">
-      <TopBar title="Misc" rightBtn="config" backBtn />
+    <div className="wrapper px-6 py-16">
+      <TopBar title="" rightBtn="config" backBtn />
+      <Markdown content={content} />
       <BottomBar appConfig={ld?.appConfig} />
     </div>
   )
