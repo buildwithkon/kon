2b47868145fcd088f09dc0d353b362171df86af0
Yuji Yamaguchi <yuji.ym@gmail.com>
2025-03-30 14:57:18 +0900

    update routes

--- templates/pwa/app/routes.ts
+++ templates/pwa/app/routes.ts
@@ -1,4 +1,10 @@
-import type { RouteConfig } from '@react-router/dev/routes'
-import { flatRoutes } from '@react-router/fs-routes'
+import { type RouteConfig, route } from '@react-router/dev/routes'
 
-export default flatRoutes() satisfies RouteConfig
+export default [
+  route('/', './routes/_index.tsx'),
+  route('home', './routes/home.tsx'),
+  route('info', './routes/info.tsx'),
+  route('qa', './routes/qa.tsx'),
+  route('forum', './routes/forum.tsx'),
+  route('misc', './routes/misc.tsx')
+] satisfies RouteConfig
