11ece2bb1c6fbb7ef61765cf36cd01408a90559a
Yuji Yamaguchi <yuji.ym@gmail.com>
2025-06-01 21:27:23 +0900

    add xmtp-node

---
+++ packages/shared/lib/xmtp-node.ts
@@ -0,0 +1,42 @@
+import {IdentifierKind, type Signer } from '@xmtp/node-sdk'
+import { toBytes, createWalletClient, http } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { baseSepolia } from 'viem/chains'
+
+interface User {
+  key: `0x${string}`;
+  account: ReturnType<typeof privateKeyToAccount>;
+  wallet: ReturnType<typeof createWalletClient>;
+}
+
+export const createUser = (key: string): User => {
+  const account = privateKeyToAccount(key as `0x${string}`);
+  return {
+    key: key as `0x${string}`,
+    account,
+    wallet: createWalletClient({
+      account,
+      chain: baseSepolia,
+      transport: http(),
+    }),
+  };
+};
+
+export const createSigner = (key: string): Signer => {
+  const sanitizedKey = key.startsWith("0x") ? key : `0x${key}`
+  const user = createUser(sanitizedKey)
+  return {
+    type: "EOA",
+    getIdentifier: () => ({
+      identifierKind: IdentifierKind.Ethereum,
+      identifier: user.account.address.toLowerCase(),
+    }),
+    signMessage: async (message: string) => {
+      const signature = await user.wallet.signMessage({
+        message,
+        account: user.account,
+      });
+      return toBytes(signature)
+    },
+  }
+}
