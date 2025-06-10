38abea9b4eaac33fb6d7842f59294c468a7ebaac
Yuji Yamaguchi <yuji.ym@gmail.com>
2025-06-10 20:26:40 +0900

    chore

--- packages/subdomain-router/src/index.ts
+++ packages/subdomain-router/src/index.ts
@@ -3,13 +3,10 @@ export default {
     const url = new URL(request.url)
     const subdomain = url.hostname.split('.')[0]
     const excludeList = env.EXCLUDE_LIST.split(',')
-    console.log('excludeList', excludeList)
     if (excludeList.includes(subdomain)) {
-      console.log('---1', 'in list')
       return fetch(request)
     }
     if (env.APP_WORKER) {
-      console.log('---2', 'no list')
       return env.APP_WORKER.fetch(request)
     }
     return new Response('Router Worker: APP_WORKER is not configured.', { status: 500 })
